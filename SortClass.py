637d2b978d0a8158f5a509da32463061171d915f
Huy Nguyen <vhuy.nguyen@sift-ag.com>
2020-05-31 10:31:44 +0700

    selection sort

--- SortClass.py
+++ SortClass.py
@@ -4,9 +4,17 @@ class SortClass:
     def __init__(self, number_arr=[4, -1, 2, 7, 6, 3, 10, -21, 44, -23, 15]):
         self.object_unsorted = number_arr
 
+    def printResult(self):
+        print(self.object_unsorted)
+
+    def swap(self, i, j):
+        temp = self.object_unsorted[i]
+        self.object_unsorted[i] = self.object_unsorted[j]
+        self.object_unsorted[j] = temp
+
     # Selection sort ~ O(n^2) Idea: Split set to 2 subsets - sorted subset and unsorted subset.
     # For each iteration, remove next element and insert to the location in sorted set
-    def selectionSort(self):
+    def insertionSort(self):
         for i, num in enumerate(self.object_unsorted):
             if i == 0:
                 continue
@@ -19,8 +27,17 @@ class SortClass:
 
             self.object_unsorted[j] = value
 
-        print(self.object_unsorted)
+    def selectionSort(self):
+        for i, num in enumerate(self.object_unsorted):
+            # find smallest element on unsorted subset
+            index = i
+            for j in range(i + 1, len(self.object_unsorted) - 1):
+                if self.object_unsorted[index] > self.object_unsorted[j]:
+                    index = j
+
+            self.swap(i, index)
 
 
 x = SortClass()
 x.selectionSort()
+x.printResult()
